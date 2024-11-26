a2bc2e76a13031e14e5eec54353f0bad9b344311
IT24104388 <it24104388@my.sliit.lk>
2024-11-26 07:54:55 +0530

    Update IT24104388Lab5Q2.java

--- Q2/IT24104388Lab2Q2.java
+++ Q2/IT24104388Lab2Q2.java
@@ -1,14 +1,45 @@
-public class IT24104388Lab2Q2 {
+import java.util.Scanner;
+
+public class IT24104388Lab5Q2 {
     public static void main(String[] args) {
-        // Given data
-        double sideLength = 10.0; // Side of the square
-        double PI = 3.14; // Value of PI
-
-        // Calculate the perimeter of the square
-        double squarePerimeter = 4 * sideLength;
-        double radius = squarePerimeter / (2 * PI);
-
-        // Display the result
-        System.out.println("Radius of the circular fence: " + radius);
+        Scanner scanner = new Scanner(System.in);
+        
+        // Prompt user to enter the number of new members introduced
+        System.out.print("Enter the number of new members introduced: ");
+        int members = scanner.nextInt();
+        
+        // Validate the input
+        if (members < 0) {
+            System.out.println("Bad Input: must be a number 0 or greater.");
+        } else {
+            // Determine the prize using a switch statement
+            String prize;
+            switch (members) {
+                case 0:
+                    prize = "No Prize";
+                    break;
+                case 1:
+                    prize = "Pen";
+                    break;
+                case 2:
+                    prize = "Umbrella";
+                    break;
+                case 3:
+                    prize = "Bag";
+                    break;
+                case 4:
+                    prize = "Travelling Chair";
+                    break;
+                default:
+                    prize = "Headphone";
+                    break;
+            }
+            
+            // Display the prize
+            System.out.println("Prize is a: " + prize);
+        }
+        
+        scanner.close();
     }
 }
+
